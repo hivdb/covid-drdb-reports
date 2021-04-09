f8859dabe5bc4a13df1e9f0b8321076227d632af
Kaiming <mads396@gmail.com>
2021-04-09 11:34:36 -0700

    update mab summary

--- table/mab/gen_table_all_mab.py
+++ table/mab/gen_table_all_mab.py
@@ -7,13 +7,17 @@ from .preset import MAB_RENAME
 SQL = """
 SELECT
     s.rx_name,
-    s.cumulative_count as count
+    s.cumulative_count as count,
+    a.availability as avail,
+    a.pdb_id as pdb,
+    t.target as target
 FROM
-    susc_results as s,
-    rx_antibodies as r
-ON
+    susc_results as s
+INNER JOIN rx_antibodies as r ON
     s.ref_name = r.ref_name
     AND s.rx_name = r.rx_name
+LEFT JOIN antibodies as a ON a.ab_name = r.ab_name
+LEFT JOIN antibody_targets as t ON a.ab_name = t.ab_name
 WHERE
     r.ab_name in (
         SELECT ab_name FROM 'antibodies'
@@ -29,27 +33,33 @@ def gen_table_all_mab(conn):
     cursor.execute(SQL)
 
     mab_group = defaultdict(list)
-    for i in cursor.fetchall():
-        rx_name = i['rx_name']
+    for rec in cursor.fetchall():
+        rx_name = rec['rx_name']
         rx_name = MAB_RENAME.get(rx_name, rx_name)
-        num = i['count']
-        mab_group[rx_name].append({
-            'rx_name': rx_name,
-            'num': num
-        })
+
+        mab_group[rx_name].append(rec)
 
     record_list = []
     for rx_name, rlist in mab_group.items():
-        count = 1
-        for rec in rlist:
-            count += rec['num']
+        count = sum([r['count'] for r in rlist] + [0])
+        target = rlist[0]['target']
+        pdb = rlist[0]['pdb']
+        avail = rlist[0]['avail']
 
         record_list.append({
             'mab': rx_name,
-            'count': count
+            'results': count,
+            'avail': avail or '',
+            'target': target or '',
+            'pdb': pdb or '',
         })
 
-    record_list.sort(key=itemgetter('count'), reverse=True)
+    record_list.sort(key=itemgetter(
+        'avail',
+        'target',
+        'results',
+        'pdb',
+        ), reverse=True)
 
     save_path = DATA_FILE_PATH / 'table_mab_figure.csv'
     dump_csv(save_path, record_list)
