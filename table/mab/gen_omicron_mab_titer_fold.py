d6ade99b6fb10c0b2823fd682eea0a189c5baf19
Kaiming <mads396@gmail.com>
2022-01-05 14:13:54 -0800

    update omicron mab figure

--- table/mab/gen_omicron_mab_titer_fold.py
+++ table/mab/gen_omicron_mab_titer_fold.py
@@ -114,36 +114,89 @@ def draw_figure(results, figure_save_path):
     cols = axes.shape[1]
 
     for row in range(rows):
+        hide_x_axis = False
+        hide_y_axis = False
+        sub_axis = False
+        if row != (rows - 1):
+            hide_x_axis = True
         for col in range(cols):
+            if col != 0:
+                hide_y_axis = True
+            if col == (cols - 1):
+                sub_axis = True
             draw_info = get_points_and_lines(results, MAB_LIST[row * 3 + col])
-            draw_sub_figure(axes[row, col], draw_info)
+            draw_sub_figure(
+                axes[row, col], draw_info, hide_x_axis, hide_y_axis, sub_axis)
 
+    fig.subplots_adjust(wspace=0, hspace=0)
     plt.savefig(str(figure_save_path), format='svg', bbox_inches='tight')
 
 
-def draw_sub_figure(ax, draw_info):
+def draw_sub_figure(
+        ax, draw_info, hide_x_axis=False, hide_y_axis=False, sub_axis=False):
 
-    ax.set_title(draw_info['mab'])
+    ax.set_title(draw_info['mab'], y=1.0, pad=-14)
 
-    ax.set_xticklabels(['WT', 'Omicron', 'fold'])
-    ax.set_ylim([0.05, 100000])
+    ax.set_xticklabels(['WT', 'Omicron', 'Fold'])
+    ax.set_ylim([0.7, 3000])
     ax.set_yscale('log', base=10)
-    ax.set_ylabel('IC50 (ng/ml) or Fold change')
+    ax.set_ylabel('IC50 (ng/ml)')
+    ax.yaxis.label.set_color('blue')
+    ax.tick_params(axis='y', colors='blue')
+
+    if hide_x_axis:
+        ax.get_xaxis().set_visible(False)
+    if hide_y_axis:
+        ax.get_yaxis().set_visible(False)
 
     for x_points, y_points in draw_info['lines']:
-        ax.plot(x_points, y_points, 'r-')
+        ax.plot(x_points, y_points, 'k-')
+
+    draw_points(ax, draw_info)
+
+    if sub_axis:
+        ax2 = ax.twinx()
+        ax2.set_ylim([0.7, 3000])
+        ax2.set_yscale('log', base=10)
+        ax2.set_ylabel('Fold change')
+        ax2.yaxis.label.set_color('red')
+        ax2.tick_params(axis='y', colors='red')
+
+        draw_points(ax2, draw_info)
+
+
+def draw_points(ax, draw_info):
+    wt_x_points = [
+        i for idx, i in enumerate(draw_info['y_points']) if (idx + 1) % 3 == 1]
+    omicron_x_points = [
+        i for idx, i in enumerate(draw_info['y_points']) if (idx + 1) % 3 == 2]
+    fold_x_points = [
+        i for idx, i in enumerate(draw_info['y_points']) if (idx + 1) % 3 == 0]
+    ax.boxplot(
+        [wt_x_points, omicron_x_points, fold_x_points],
+        positions=[0, 1, 2],
+        labels=['WT', 'Omicron', 'Fold'],
+        showfliers=False,
+        boxprops={'color': 'gray'},
+        capprops={'color': 'gray'},
+        whiskerprops={'color': 'gray'})
 
     for idx, (xp, yp, m) in enumerate(zip(
             draw_info['x_points'],
             draw_info['y_points'],
             draw_info['markers'])):
 
-        if m == 'filled':
-            ax.scatter(
-                [xp], [yp], marker='o', facecolors='b', edgecolors='b')
+        if (idx + 1) % 3 == 0:
+            color = 'r'
         else:
-            ax.scatter(
-                [xp], [yp], marker='o', facecolors='none', edgecolors='b')
+            color = 'b'
+
+        ax.scatter(
+                [xp], [yp], marker=m, facecolors=color, edgecolors=color)
+
+
+
+
 
 
 def get_points_and_lines(records, mab):
@@ -172,7 +225,7 @@ def get_points_and_lines(records, mab):
                 test['type'] = 'Omicron'
                 test['value'] = v
             if k == 'fold':
-                fold['type'] = 'fold'
+                fold['type'] = 'Fold'
                 fold['value'] = v
             if k == 'control_ic50_cmp':
                 control['cmp'] = v
@@ -182,6 +235,14 @@ def get_points_and_lines(records, mab):
                 fold['cmp'] = v
 
         for rec in [control, test, fold]:
+            if rec['value'] > 1000:
+                rec['value'] = 1000
+                rec['cmp'] = '>'
+
+            if rec['value'] < 1:
+                rec['value'] = 1
+                rec['cmp'] = '='
+
             for k, v in rec.items():
                 if k == 'type':
                     x_points.append(v)
@@ -189,9 +250,9 @@ def get_points_and_lines(records, mab):
                     y_points.append(v)
                 if k == 'cmp':
                     if v == '=':
-                        markers.append('filled')
+                        markers.append('o')
                     else:
-                        markers.append('void')
+                        markers.append('^')
 
         lines.append((
             x_points[-3:-1],
