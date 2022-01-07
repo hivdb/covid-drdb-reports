bc7018dfffdfb8fcec3508de3b544bc2526a03c1
Kaiming <mads396@gmail.com>
2022-01-06 16:35:05 -0800

    update gen_omicron_s_pattern

--- table/mab/gen_omicron_s_pattern.py
+++ table/mab/gen_omicron_s_pattern.py
@@ -1,16 +1,26 @@
 from preset import DATA_FILE_PATH
 from preset import dump_csv
 import re
+from collections import defaultdict
 
 
 SUMMARY_SQL = """
 SELECT DISTINCT
     s.ref_name,
-    iso.pattern
+    iso.pattern,
+    CASE
+        WHEN iso_meta.gisaid_id IS NOT NULL THEN
+            iso_meta.iso_name
+        WHEN iso_meta.genbank_accn IS NOT NULL THEN
+            iso_meta.iso_name
+        ELSE
+            ''
+    END iso_name
 FROM
     susc_results_view s,
     {rx_type} rx,
-    {iso_type} iso
+    {iso_type} iso,
+    isolates iso_meta
 WHERE
     s.ref_name = rx.ref_name
     AND
@@ -19,6 +29,8 @@ WHERE
     s.iso_name = iso.iso_name
     AND
     iso.var_name = 'Omicron'
+    AND
+    iso.iso_name = iso_meta.iso_name
 ;
 """
 
@@ -38,10 +50,12 @@ def gen_omicron_s_pattern(
     results = []
     mut_pos_list = set()
     pos_ref_aa_dict = {}
+    pos_mut_stat = {}
     for row in cursor.fetchall():
         ref_name = row['ref_name']
+        iso_name = row['iso_name']
         pattern = row['pattern']
-        results.append([ref_name, pattern])
+        results.append([ref_name, iso_name, pattern])
 
         mut_list = pattern.split('+')
         for mut in mut_list:
@@ -55,17 +69,29 @@ def gen_omicron_s_pattern(
             mut_pos_list.add(pos)
             pos_ref_aa_dict[pos] = ref_aa
 
+            if pos not in pos_mut_stat:
+                pos_mut_stat[pos] = defaultdict(int)
+            pos_mut_stat[pos][mut_aa] += 1
+
     mut_pos_list = sorted(list(mut_pos_list))
 
-    headers = ['ref_name']
+    pos_most_common_mut_aa = {}
+    for pos, mut_aa_stat in pos_mut_stat.items():
+        mut_aa_stat = [(k, v) for k, v in mut_aa_stat.items() if k]
+        most_common_mut_aa = sorted(mut_aa_stat, key=lambda x: x[1])[-1][0]
+        pos_most_common_mut_aa[pos] = most_common_mut_aa
+
+    headers = ['ref_name', 'iso_name']
     for pos in mut_pos_list:
         ref_aa = pos_ref_aa_dict[pos]
-        headers.append('{}{}'.format(ref_aa, pos))
+        mut_aa = pos_most_common_mut_aa[pos]
+        headers.append('{}{}{}'.format(ref_aa, pos, mut_aa))
 
     summary_results = []
-    for ref_name, pattern in results:
+    for ref_name, iso_name, pattern in results:
         result = {
-            'ref_name': ref_name
+            'ref_name': ref_name,
+            'iso_name': iso_name
         }
 
         mut_list = pattern.split('+')
@@ -75,7 +101,13 @@ def gen_omicron_s_pattern(
             if '-' in pos:
                 pos = pos.split('-', 1)[0]
 
-            result['{}{}'.format(ref_aa, pos)] = mut_aa
+            pos = int(pos)
+
+            most_common_mut_aa = pos_most_common_mut_aa[pos]
+
+            result['{}{}{}'.format(ref_aa, pos, most_common_mut_aa)] = (
+                '-' if most_common_mut_aa == mut_aa else mut_aa
+            )
 
         summary_results.append(result)
 
