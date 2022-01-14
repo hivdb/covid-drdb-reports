51dbfee059e4b5f567b9b194fa2658f1ee8f9da7
Kaiming <mads396@gmail.com>
2022-01-14 14:04:31 -0800

    update gen_omicron_s_pattern

--- table/mab/gen_omicron_s_pattern.py
+++ table/mab/gen_omicron_s_pattern.py
@@ -1,3 +1,4 @@
+from enum import unique
 from preset import DATA_FILE_PATH
 from preset import dump_csv
 import re
@@ -7,30 +8,36 @@ from collections import defaultdict
 SUMMARY_SQL = """
 SELECT DISTINCT
     s.ref_name,
-    iso.pattern,
+    ctl.var_name control_name,
+    test.pattern,
     CASE
-        WHEN iso_meta.gisaid_id IS NOT NULL THEN
-            iso_meta.iso_name
-        WHEN iso_meta.genbank_accn IS NOT NULL THEN
-            iso_meta.iso_name
+        WHEN test_meta.gisaid_id IS NOT NULL THEN
+            test_meta.iso_name
+        WHEN test_meta.genbank_accn IS NOT NULL THEN
+            test_meta.iso_name
         ELSE
             ''
     END iso_name
 FROM
     susc_results_view s,
     {rx_type} rx,
-    {iso_type} iso,
-    isolates iso_meta
+    isolates ctl,
+    {iso_type} test,
+    isolates test_meta
 WHERE
     s.ref_name = rx.ref_name
     AND
     s.rx_name = rx.rx_name
+
     AND
-    s.iso_name = iso.iso_name
+    s.control_iso_name = ctl.iso_name
+
     AND
-    iso.var_name = 'Omicron'
+    s.iso_name = test.iso_name
     AND
-    iso.iso_name = iso_meta.iso_name
+    test.var_name = 'Omicron'
+    AND
+    test.iso_name = test_meta.iso_name
 ;
 """
 
@@ -53,9 +60,10 @@ def gen_omicron_s_pattern(
     pos_mut_stat = {}
     for row in cursor.fetchall():
         ref_name = row['ref_name']
+        control_name = row['control_name']
         iso_name = row['iso_name']
         pattern = row['pattern']
-        results.append([ref_name, iso_name, pattern])
+        results.append([ref_name, control_name, iso_name, pattern])
 
         mut_list = pattern.split('+')
         for mut in mut_list:
@@ -76,21 +84,28 @@ def gen_omicron_s_pattern(
     mut_pos_list = sorted(list(mut_pos_list))
 
     pos_most_common_mut_aa = {}
+    pos_uniq_mut_aa = []
     for pos, mut_aa_stat in pos_mut_stat.items():
-        mut_aa_stat = [(k, v) for k, v in mut_aa_stat.items() if k]
+        mut_aa_stat = [(k, v) for k, v in mut_aa_stat.items()]
+        if len(mut_aa_stat) == 1 and mut_aa_stat[0][-1] == len(results):
+            pos_uniq_mut_aa.append(pos)
         most_common_mut_aa = sorted(mut_aa_stat, key=lambda x: x[1])[-1][0]
         pos_most_common_mut_aa[pos] = most_common_mut_aa
 
-    headers = ['ref_name', 'iso_name']
+    headers = ['ref_name', 'control_name', 'iso_name']
     for pos in mut_pos_list:
+        if pos in pos_uniq_mut_aa:
+            continue
+
         ref_aa = pos_ref_aa_dict[pos]
         mut_aa = pos_most_common_mut_aa[pos]
         headers.append('{}{}{}'.format(ref_aa, pos, mut_aa))
 
     summary_results = []
-    for ref_name, iso_name, pattern in results:
+    for ref_name, control_name, iso_name, pattern in results:
         result = {
             'ref_name': ref_name,
+            'control_name': control_name,
             'iso_name': iso_name
         }
 
@@ -102,6 +117,8 @@ def gen_omicron_s_pattern(
                 pos = pos.split('-', 1)[0]
 
             pos = int(pos)
+            if pos in pos_uniq_mut_aa:
+                continue
 
             most_common_mut_aa = pos_most_common_mut_aa[pos]
 
@@ -110,5 +127,7 @@ def gen_omicron_s_pattern(
             )
 
         summary_results.append(result)
+
+    summary_results.sort(key=lambda x: x['ref_name'])
 
     dump_csv(csv_save_path, summary_results, headers=headers)
